c99623d1cded094944dd6d71c0d77f726d1771cd
Levenho <schwarznick08@gmail.com>
2025-05-10 20:19:31 +0200

    Added UI

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import json
 import keyboard
+import tkinter as tk
 import time
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
@@ -14,9 +15,8 @@ load_dotenv()
 with open("config.json") as config:
     config = json.load(config)
 
-#Get hotkeys from config
+#Get hotkey from config
 hotkey_addtoPlaylist = str(config["hotkey_addtoPlaylist"])
-hotkey_pause = str(config["hotkey_pause"])
 
 #Set needed permissions
 scope = ["user-read-currently-playing", "user-library-modify", "user-library-read"]
@@ -29,38 +29,72 @@ redirect_uri = os.getenv("redirect_uri")
 #Create client with environment variables
 client = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri))
 
-while True:
-    #Adding the current song to the "Liked Songs" playlist
-    if keyboard.is_pressed(hotkey=hotkey_addtoPlaylist):
+#UI
+root = tk.Tk()
+root.title("SpotyAdd")
+root.geometry("350x200")
+#CaptureInput
+start_button = tk.Button(root, text="Start", background="white", command=lambda: SpotyAdd.AddHotkey())
+start_button.pack()
+#StopInput
+pause_button = tk.Button(root, text="Pause",  background="white", command=lambda: SpotyAdd.RemoveHotkeyForPause())
+pause_button.pack()
+#Exit
+exit_button = tk.Button(root, text="Exit",  background="white", command=lambda: root.quit())
+exit_button.pack()
+
+CaptureInput = False
+StopInput = False
+
+class SpotyAdd():
+    def AddHotkey():
+        global CaptureInput, StopInput
+        CaptureInput = True
+        StopInput = False
+        #Add hotkey
+        keyboard.add_hotkey(hotkey=hotkey_addtoPlaylist, callback=SpotyAdd.OnHotkey)
+        #Set button states
+        if CaptureInput == True:
+            pause_button.config(state="normal")
+            start_button.config(state="disabled")
+    
+    def OnHotkey():
         #Check if spotify is running or there is a song being played
-        try:
-            #Get the current song
-            current_song = client.currently_playing(market=None, additional_types=None)
-            #Extract trackId and put it into the trackURL list
-            trackId = current_song["item"]["id"] 
-            trackURL = [f"http://open.spotify.com/track/{trackId}"]
-            trackName = current_song["item"]["name"]
-            #Check if current song is already in "Liked Songs" playlist
-            if client.current_user_saved_tracks_contains(tracks=trackURL)[0] == False:
-                #Add current song to the "Liked Songs" playlist
-                client.current_user_saved_tracks_add(tracks=trackURL)
-                get_time = datetime.now()
-                new_time = get_time.strftime("%H:%M:%S")
-                current_time = new_time.replace("'", "")
-                print(f"[{current_time}] {trackName} was added to Liked Songs.")
-                continue
-            else:
-                get_time = datetime.now()
-                new_time = get_time.strftime("%H:%M:%S")
-                current_time = new_time.replace("'", "")
-                print(f"[{current_time}] {trackName} is already in Liked Songs.")
-                continue
-        except:
-            print("Spotify is not running or there is no song playing.")
-    #Pause the program
-    if keyboard.is_pressed(hotkey=hotkey_pause):
-        print("Program is paused!")
-        keyboard.wait(hotkey=hotkey_pause)
-        print("Unpaused!")
-        time.sleep(0.2)
-        
+            try:
+                #Get the current song
+                current_song = client.currently_playing(market=None, additional_types=None)
+                #Extract trackId and put it into the trackURL list
+                trackId = current_song["item"]["id"] 
+                trackURL = [f"http://open.spotify.com/track/{trackId}"]
+                trackName = current_song["item"]["name"]
+                #Check if current song is already in "Liked Songs" playlist
+                if client.current_user_saved_tracks_contains(tracks=trackURL)[0] == False:
+                    #Add current song to the "Liked Songs" playlist
+                    client.current_user_saved_tracks_add(tracks=trackURL)
+                    get_time = datetime.now()
+                    new_time = get_time.strftime("%H:%M:%S")
+                    current_time = new_time.replace("'", "")
+                    print(f"[{current_time}] {trackName} was added to Liked Songs.")
+                    time.sleep(1)
+                else:
+                    get_time = datetime.now()
+                    new_time = get_time.strftime("%H:%M:%S")
+                    current_time = new_time.replace("'", "")
+                    print(f"[{current_time}] {trackName} is already in Liked Songs.")
+                    time.sleep(1)
+            except:
+                print("Spotify is not running or there is no song playing.")
+                time.sleep(1)
+
+    def RemoveHotkeyForPause():
+        global CaptureInput, StopInput
+        CaptureInput = False
+        StopInput = True
+        #Remove hotkey
+        keyboard.remove_hotkey(hotkey_or_callback=hotkey_addtoPlaylist)
+        #Set button states
+        if CaptureInput == False:
+            start_button.config(state="normal")
+            pause_button.config(state="disabled")
+
+root.mainloop()
